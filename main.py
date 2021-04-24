9cd261ec3c3b13c816952a326646e8bc4d2cd114
Matt <duvallma@oregonstate.edu>
2021-04-24 10:01:04 -0600

    starter code

---
+++ main.py
@@ -0,0 +1,18 @@
+from google.cloud import datastore
+from flask import Flask, request
+import json
+import constants
+import lodging
+import guest
+
+app = Flask(__name__)
+app.register_blueprint(lodging.bp)
+app.register_blueprint(guest.bp)
+client = datastore.Client()
+
+@app.route('/')
+def index():
+    return "Please navigate to /lodgings to use this API"
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
